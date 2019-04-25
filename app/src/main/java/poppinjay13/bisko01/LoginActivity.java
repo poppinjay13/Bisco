a3fe9c78362fe480f3a33c480783414247693f9b
Ian Odundo <thisian13@gmail.com>
2019-04-25 19:51:39 +0300

    Default activity
    Change from scanner to maps

--- app/src/main/java/poppinjay13/bisko01/LoginActivity.java
+++ app/src/main/java/poppinjay13/bisko01/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if(user!=null){
-                    Intent intent = new Intent(LoginActivity.this, ScannerActivity.class);
+                    Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
                     startActivity(intent);
                     finish();
                     return;
