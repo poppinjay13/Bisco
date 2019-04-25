3c69f961ecd33e663170d6b1554d3b7a5f863a87
Daniel Olamide <dannyolamide@gmail.com>
2019-04-25 17:10:30 +0300

    I've messed it up

--- app/src/main/java/poppinjay13/bisko01/MapsActivity.java
+++ app/src/main/java/poppinjay13/bisko01/MapsActivity.java
@@ -141,7 +141,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //move map camera
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
 
         //stop location updates
         if (mGoogleApiClient != null) {
