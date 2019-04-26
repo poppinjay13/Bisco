658d3d74132f0947fd303acca87d4a3d9cd2bada
Sinwa <sianwamak@gmail.com>
2019-04-26 11:06:50 +0300

    Update Payment.java

--- app/src/main/java/poppinjay13/bisko01/Payment.java
+++ app/src/main/java/poppinjay13/bisko01/Payment.java
@@ -69,7 +69,7 @@ public class Payment extends AppCompatActivity {
                 "174379",
                 "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
                 TransactionType.CustomerBuyGoodsOnline,
-                "10",
+                "50",
                 "254708374149",
                 "174379",
                 phonenumber,
